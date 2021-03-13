c4defaa31feb379757d07a1f76d7a2bfecf5a9a6
Ila-bura <ila.bura@gmail.com>
2021-03-13 16:15:31 +0000

    Time updated

--- src/app.js
+++ src/app.js
@@ -1,25 +1,24 @@
-let now = new Date();
 
-      let h1 = document.querySelector("h1");
+ function formatDate(timestamp)  {
+   let date = new Date(timestamp);
+   let hours = date.getHours();
+   let minutes = date.getMinutes();
+   if (minutes < 10) {
+     minutes = `0${minutes}`;
+   } 
 
-      
-      let hours = now.getHours();
-      if (hours < 10){
-        hours = `0${hours}`;
-      }
-      let minutes = now.getMinutes();
-      if (minutes < 10){
-        minutes = `0${minutes}`;
-      }
-      
+   if (hours < 10) {
+     hours = `0${hours}`;
+   } 
+   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+ }   
 
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      let day = days[now.getDay()];
-      h1.innerHTML = `${day}, ${hours}:${minutes}`;
 
-      
+
 function displayCurrentWeather(response) {
-  console.log (response.data.main.temp)
+  
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -30,6 +29,9 @@ function displayCurrentWeather(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
+   
+    document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
+
 }
 
 function searchCity(city) {
