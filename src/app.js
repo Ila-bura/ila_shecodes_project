7dca4cd5d110998161b431434ae59d4450739fc0
Ila-bura <ila.bura@gmail.com>
2021-03-13 16:59:19 +0000

    Added weather icon from API

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ return `${day} ${hours}:${minutes}`;
 
 function displayCurrentWeather(response) {
   
+  
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -32,6 +33,8 @@ function displayCurrentWeather(response) {
    
     document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
 
+    document.querySelector("#icon").setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+icon.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
